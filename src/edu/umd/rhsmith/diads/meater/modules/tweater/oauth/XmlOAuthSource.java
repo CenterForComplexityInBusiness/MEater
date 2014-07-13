0a8c373bdcd74052193053fce4a333facf95f0bb
rmachedo <rmachedo@gmail.com>
2014-07-13 15:48:01 -0400

    Add basic implementation of XmlOAuthSource

--- src/edu/umd/rhsmith/diads/meater/modules/tweater/oauth/XmlOAuthSource.java
+++ src/edu/umd/rhsmith/diads/meater/modules/tweater/oauth/XmlOAuthSource.java
@@ -1,12 +1,18 @@
 package edu.umd.rhsmith.diads.meater.modules.tweater.oauth;
 
+import org.apache.commons.configuration.XMLConfiguration;
 
 public class XmlOAuthSource implements OAuthSource {
 
 	@Override
 	public OAuthInfo getOAuthInfo(String name) throws OAuthLoadException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			XMLConfiguration xml = new XMLConfiguration(name + ".xml");
+			OAuthConfig oAuth = new OAuthConfig();
+			oAuth.loadConfigurationFrom(xml);
+			return oAuth;
+		} catch (Exception e) {
+			throw new OAuthLoadException(e);
+		}
 	}
-
 }
