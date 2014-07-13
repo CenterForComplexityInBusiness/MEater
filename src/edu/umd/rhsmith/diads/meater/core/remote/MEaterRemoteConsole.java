d1c36a97d47893312196801f945b014fbbd47efe
rmachedo <rmachedo@gmail.com>
2014-07-13 09:35:33 -0400

    Add shell of remote console's main()

--- src/edu/umd/rhsmith/diads/meater/core/remote/MEaterRemoteConsole.java
+++ src/edu/umd/rhsmith/diads/meater/core/remote/MEaterRemoteConsole.java
@@ -3,8 +3,40 @@ package edu.umd.rhsmith.diads.meater.core.remote;
 public class MEaterRemoteConsole {
 
 	public static void main(String[] args) {
+		if (args.length == 0) {
+			printUsage();
+			return;
+		}
+
+		String command = args[0];
+
+		if (command.equals("start")) {
+			startMEater(args);
+		} else if (command.equals("list")) {
+			listMEaters(args);
+		} else if (command.equals("stop")) {
+			stopMEater(args);
+		} else {
+			printUsage();
+		}
+	}
+
+	private static void startMEater(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
+	private static void listMEaters(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void stopMEater(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void printUsage() {
+		// TODO Auto-generated method stub
+	}
 }
