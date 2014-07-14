376efe43bfa704cced41d59bb8c0d64b31db8094
rmachedo <rmachedo@gmail.com>
2014-07-14 10:42:14 -0400

    Removed unused ComponentClass annotation

--- src/edu/umd/rhsmith/diads/meater/core/app/components/ComponentClass.java
+++
@@ -1,9 +0,0 @@
-package edu.umd.rhsmith.diads.meater.core.app.components;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface ComponentClass {
-	String foo();
-}
