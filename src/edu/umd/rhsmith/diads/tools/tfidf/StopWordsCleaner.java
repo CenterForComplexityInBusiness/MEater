d84d6023acfe170bdff5e0a36dc12988b54e2cc9
rmachedo <rmachedo@gmail.com>
2014-07-18 18:39:51 -0400

    Add stopwords file loading

--- src/edu/umd/rhsmith/diads/tools/tfidf/StopWordsCleaner.java
+++ src/edu/umd/rhsmith/diads/tools/tfidf/StopWordsCleaner.java
@@ -1,7 +1,11 @@
 package edu.umd.rhsmith.diads.tools.tfidf;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class StopWordsCleaner implements TermCleaner {
 
@@ -18,5 +22,26 @@ public class StopWordsCleaner implements TermCleaner {
 		} else {
 			return analysisText;
 		}
+	}
+
+	public static Collection<String> stopWordsFromFile(String filename)
+			throws IOException {
+		return stopWordsFromFile(new File(filename));
+	}
+
+	public static Collection<String> stopWordsFromFile(File file)
+			throws IOException {
+		Collection<String> words = new LinkedList<String>();
+		Scanner s = new Scanner(file);
+		while (s.hasNextLine()) {
+			String line = s.nextLine();
+			if (line.startsWith("#")) {
+				continue;
+			}
+			line = line.trim();
+			words.add(line);
+		}
+		s.close();
+		return words;
 	}
 }
